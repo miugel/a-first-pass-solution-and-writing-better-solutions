1af4fc298efbe40722e7170ae0982c1455f11df5
Miguel <miguelqnicolas@gmail.com>
2020-04-15 18:43:22 -0400

    Complete recipe_batches exercise

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,10 +1,22 @@
-#!/usr/bin/python
-
-import math
+'''
+- loop dictionary comparing values
+- 
+'''
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  batches = 9999
 
+  for i in recipe:
+      if ingredients[i] // recipe[i] < batches:
+        batches = ingredients[i] // recipe[i]
+  
+  return batches
+
+print(recipe_batches({ 'milk': 2, 'sugar': 40, 'butter': 20 }, { 'milk': 5, 'sugar': 120, 'butter': 500 }))
+
+'''
+#!/usr/bin/python
+import math
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
@@ -12,3 +24,4 @@ if __name__ == '__main__':
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+'''
