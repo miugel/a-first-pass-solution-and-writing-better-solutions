a65a6201e804834a7cb1cadfe9a3fedece91a561
Miguel <miguelqnicolas@gmail.com>
2020-04-15 14:58:39 -0400

    Clean up files and add notes

---
+++ a-first-pass-solution-notes.py
@@ -0,0 +1,21 @@
+'''
+binary search
+insertion sort
+selection sort
+bubble sort
+count sort
+quick sort
+merge sort
+
+first-pass solution
+    starting point, mvp, something that works
+
+caching, memoization
+trading space for time
+you can buy space, but you cant buy time
+
+out-of-place, memory
+
+space and time complexity
+
+'''
